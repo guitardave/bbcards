0a018398f0de0d61b93174c0aaa935f774e41d39
David Crandell <david@onholdwizard.com>
2021-08-18 19:17:25 -0500

    player views and fixed a few other items

--- cards/forms.py
+++ cards/forms.py
@@ -1,13 +1,13 @@
 from django import forms
 from .models import *
 
-class CardForm(models.ModelForm):
+class CardForm(forms.ModelForm):
 	class Meta:
 		model = Card
 		fields = "__all__"
         
         
-class CardSetForm(models.ModelForm):
+class CardSetForm(forms.ModelForm):
     class Meta:
         model = CardSet
         fields = "__all__"
--- players/forms.py
+++ players/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import *
 
-class PlayerForm(forms.Form):
+class PlayerForm(forms.ModelForm):
 	class Meta:
 		model = Player
 		fields = '__all__'
