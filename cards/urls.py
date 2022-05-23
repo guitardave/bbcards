25b491cd554955aef0773f72a7e375bb9a6953c8
David Crandell <david@onholdwizard.com>
2022-05-23 08:10:54 -0500

    update views for cards; consolidate code

--- cards/urls.py
+++ cards/urls.py
@@ -4,12 +4,12 @@ from . import views
 app_name = 'cards'
 
 urlpatterns = [
-    path('', views.card_list, name='card-list-all'),
-    path('sets/', views.card_set_list, name='cardsets'),
+    path('', views.CardsListView.as_view(), name='card-list-all'),
+    path('sets/', views.CardSetList.as_view(), name='cardsets'),
     path('sets/new/', views.CardSetCreate.as_view(), name='cardsets-new'),
     path('sets/<slug:slug>/update/', views.CardSetUpdate.as_view(), name='cardsets-upd'),
     path('cards/search', views.card_search, name='card-search'),
-    path('cards/<slug>/player/', views.CardsViewPLayer.as_view(), name='card-list-player'),
+    path('cards/<slug>/player/', views.cards_list_player, name='card-list-player'),
     path('cards/<slug>/set/', views.card_list, name='card-list'),
     path('cards/new/', views.CardCreate.as_view(), name='card-new-all'),
     path('cards/<slug>/new/', views.CardNewSet.as_view(), name='card-new-set'),
