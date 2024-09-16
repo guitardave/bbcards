fd7234566e670e4cb51a6b6d1aa63158de7a6f27
dcrandell <david@onholdwizard.com>
2024-09-15 18:50:15 -0700

    clean up profile page/profile update form

--- users/forms.py
+++ users/forms.py
@@ -1,10 +1,19 @@
 from django import forms
+
+from players.models import Player
 from .models import CardUser
 
 
 class UserForm(forms.ModelForm):
+    email = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control form-control-lg'}))
     username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control form-control-lg'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control form-control-lg'}))
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control form-control-lg'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control form-control-lg'}))
+    favorite_player = forms.ModelChoiceField(
+        widget=forms.Select(attrs={'class': 'form-control form-control-lg'}),
+        queryset=Player.objects.all()
+    )
 
     class Meta:
         model = CardUser
